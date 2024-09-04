df2be602368e47dac9dfe6985d5d7354e33c7e5d
Ariel2098 <Arielwgp-98@outlook.com>
2024-09-03 21:07:35 -0500

    creacion de logica de tabla usuarios y filtros

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from "@angular/core";
-import { Firestore, collection, query, where, getDocs } from "@angular/fire/firestore";
+import { Firestore, collection, query, where, getDocs, collectionData, QuerySnapshot } from "@angular/fire/firestore";
+import { from, Observable } from "rxjs";
 
 @Injectable({
     providedIn: 'root'  
@@ -17,5 +18,31 @@ export class AuthService {
             }else{
                 return false;
             }
+        }
+
+        getUsers(): Observable<any[]>{
+            const usersCollection = collection(this.firestore, 'users');
+            return collectionData(usersCollection, {idField: 'id'});
+        }
+
+        getFilteredUsers(nombre: string, pais: string, apodo:string): Observable<any[]>{
+            let usersCollection = collection(this.firestore, 'users');
+            let q = query(usersCollection);
+
+            if (nombre){
+                q = query(q, where('name','==',nombre));
+            }
+            
+            if (pais){
+                q = query(q, where('country','==',pais));
+            }
+
+            if (apodo){
+                q = query(q, where('nickname','==',apodo));
+            }
+
+            return from(getDocs(q).then(QuerySnapshot => 
+                QuerySnapshot.docs.map(doc => doc.data())
+            ));
         }
 }
