11e348c5a9f9d91e195f823c25dba0220b7f8218
Ariel <122324599+WilsonGavilanez98@users.noreply.github.com>
2024-09-18 20:06:49 +0000

    Cambios en los componentes de menu y usuarios

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,19 +1,20 @@
 import { Component, Output, EventEmitter } from '@angular/core';
 
-
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrl: './menu.component.css'
+  styleUrls: ['./menu.component.css']
 })
 export class MenuComponent {
 
   isActive = false;
+  isCollapsed = false; // Nueva variable para controlar el estado colapsado
 
   @Output() ActiveChanged = new EventEmitter<boolean>();
 
   toggleSidebar(){
     this.isActive = !this.isActive;
+    this.isCollapsed = this.isActive ? !this.isCollapsed : false; // Alterna entre colapsado y expandido
     this.ActiveChanged.emit(this.isActive)
   }
 }
